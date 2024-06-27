5eb94a0b8a6b19a9bd5d4aa3eb9ec083af992e1d
debashishtiu911 <debashishtiu14@gmail.com>
2024-06-27 15:23:16 +0530

    portfolio

---
+++ src/components/Footer.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import {
+  AiFillGithub,
+  AiOutlineTwitter,
+  
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+
+function Footer() {
+  let date = new Date();
+  let year = date.getFullYear();
+  return (
+    <Container fluid className="footer  flex">
+      <Row >
+        <Col md="4" className="footer-copywright">
+          <h3>Designed and Developed by Debashish Tiu</h3>
+        </Col>
+        <Col md="4" className="footer-copywright">
+          <h3>Copyright © {year} SB</h3>
+        </Col>
+        <Col md="4" className="footer-body">
+          <ul className="footer-icons flex mx-4">
+            <li className="social-icons ">
+              <a
+                href="https://github.com/debashishtiu911"
+                style={{ color: "white" }}
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                <AiFillGithub />
+              </a>
+            </li>
+            <li className="social-icons">
+              <a
+                href="https://twitter.com"
+                style={{ color: "white" }}
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                <AiOutlineTwitter />
+              </a>
+            </li>
+            <li className="social-icons">
+              <a
+                href="https://www.linkedin.com/in/debashish-tiu-34a742249"
+                style={{ color: "white" }}
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                <FaLinkedinIn />
+              </a>
+            </li>
+            
+          </ul>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default Footer;
---
+++ src/components/Techno.jsx
@@ -0,0 +1,124 @@
+
+import React from 'react'
+import { RiReactjsLine } from 'react-icons/ri'
+import { TbBrandNextjs } from 'react-icons/tb'
+import { SiMongodb } from 'react-icons/si'
+import { FaNodeJs } from 'react-icons/fa'
+import { BiLogoPostgresql } from 'react-icons/bi'
+import {animate, motion} from 'framer-motion';
+
+ const iconVariants=(duration) =>({
+    initial:{ y :-10},
+    animate :{
+        y:[10,-10],
+        transition:{
+        duration:duration,
+        ease : "linear",
+        repeat: Infinity,
+        repeatType :"reverse"
+        }
+
+    }
+    
+
+ })
+const Techno = () => {
+  return (
+    <div className='border-b border-neutral-800 pb-24'>
+        
+    <motion.h1 
+    whileInView={{opacity:1 ,y:0}}
+    initial={{opacity:0,y:-100}}
+    transition={{duration:1.5}} 
+    className='my-20 text-center text-4xl '>
+        Technologies
+        </motion.h1>
+        <motion.div
+        whileInView={{opacity:1 ,x:0}}
+        initial={{opacity:0,x:-100}}
+        transition={{duration:1.5}} 
+         className="flex flex-wrap items-center justify-center gap-4">
+        <motion.div 
+        variants ={iconVariants(2)}
+        initial="initial"
+        animate="animate"
+
+        className="rounded-2xl border-4 border-neutral-800 p-4">
+        <RiReactjsLine className='text-7xl text-cyan-400'/>
+            </motion.div>
+            <motion.div 
+        variants ={iconVariants(3)}
+        initial="initial"
+        animate="animate" className="rounded-2xl border-4 border-neutral-800 p-4">
+        <TbBrandNextjs className='text-7xl '/>
+            </motion.div>
+            <motion.div 
+        variants ={iconVariants(3.5)}
+        initial="initial"
+        animate="animate" className="rounded-2xl border-4 border-neutral-800 p-4">
+        <SiMongodb className='text-7xl text-green-400'/>
+            </motion.div>
+            <motion.div 
+        variants ={iconVariants(4)}
+        initial="initial"
+        animate="animate" className="rounded-2xl border-4 border-neutral-800 p-4">
+        <FaNodeJs className='text-7xl text-red-400'/>
+            </motion.div>
+            <motion.div 
+        variants ={iconVariants(4.5)}
+        initial="initial"
+        animate="animate" className="rounded-2xl border-4 border-neutral-800 p-4">
+        <BiLogoPostgresql className='text-7xl text-sky-700'/>
+            </motion.div>
+
+
+        </motion.div>
+
+        
+        </div>
+  )
+}
+
+export default Techno
+
+// import React from 'react'
+// import { RiReactjsLine } from 'react-icons/ri'
+// import { RiNextjsFill } from "react-icons/ri";
+// import { SiMongodb } from "react-icons/si";
+// import { FaNodeJs } from "react-icons/fa";
+// import { BiLogoPostgresql } from 'react-icons/ri'
+// //import { RiReactjsLine } from 'react-icons/ri'
+
+// const Techno = () => {
+//   return (
+//     <div className='border-b border-neutral-800 pb-24'>
+//         <h2 className="my-20 text-4xl text-center" >
+
+//         Technologies
+//         </h2>
+//         <div className="flex flex-wrap items-center justify-center gap-4">
+//             <div className="rounded-2xl border-4 border-neutral-800 p-4">
+//                 <RiReactjsLine className='text-7xl text-cyan-400'/>
+//             </div>
+//             <div className="rounded-2xl border-4 border-neutral-800 p-4">
+//                 <RiNextjsFill className='text-7xl text-cyan-400'/>
+//             </div>
+//             <div className="rounded-2xl border-4 border-neutral-800 p-4">
+//                 <SiMongodb className='text-7xl text-cyan-400'/>
+//             </div>
+//             <div className="rounded-2xl border-4 border-neutral-800 p-4">
+//                 <FaNodeJs className='text-7xl text-cyan-400'/>
+//             </div>
+//             <div className="rounded-2xl border-4 border-neutral-800 p-4">
+//                 <BiLogoPostgresql className='text-7xl text-cyan-400'/>
+//             </div>
+//             <div className="rounded-2xl border-4 border-neutral-800 p-4">
+//                 <RiReactjsLine className='text-7xl text-cyan-400'/>
+//             </div>
+//         </div>
+
+//         </div>
+//   )
+// }
+
+// export default Techno
